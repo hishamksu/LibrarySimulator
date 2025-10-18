02b9bd828b5aa166a1bbf367ec61c79de789cebd
Saeed Alghamdi <Saeed Alghamdi@Saheedov>
2025-10-18 13:05:14 +0300

    Selection Menu

--- LibrarySimulator/src/LibrarySimulator.java
+++ LibrarySimulator/src/LibrarySimulator.java
@@ -1,9 +1,46 @@
+import java.util.Scanner;
 
 public class LibrarySimulator {
-	 
+
 	public static void main(String[] args) {
-		System.out.print("WELCOME TO OUR PROJECT");
-		System.out.print("WELCOME TO OUR PROJECT");
+		System.out.println("WELCOME TO THE BOOK RENTAL SYSTEM");
+		System.out.println(
+				"Please select an option:\n"
+				+ "1-Login as Hisham: 44*******\n"
+				+ "2-Login as Mohammed: 44*******\n"
+				+ "3-Login as Saeed: 44*******\n"
+				+ "4-Login as Administrator\n"
+				+ "5-Exit the program");
+
+		Scanner input = new Scanner(System.in);
+		int choice = input.nextInt();
+
+		switch (choice) {
+		case 1:
+			
+			
+			break;
+		case 2:
+		
+			
+			break;
+		case 3:
+			
+			
+			break;
+		case 4:
+			
+			
+			break;
+		case 5:	
+			
+			
+		default:
+			System.out.print("Invalid selection.");
+			break;
+
+		}
+
 	}
 
 }
